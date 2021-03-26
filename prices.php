c5e0f9a65a2c85135e05416d2230af2ed5011634
daniel0270 <daniel.eriksson15@outlook.com>
2021-03-26 08:59:32 +0100

    servertest

--- prices.php
+++ prices.php
@@ -39,7 +39,49 @@ require_once("include/CApp.php");
     <div class="content">
         <div id="main">
             <h1> PRISER</h1>
+            <?php
+////////////////////////////// SERVER TEST //////////////////////////////////
 
+$server = "localhost";
+$username = "root";
+$password = "root";
+$dbname = "taxilernbo";
+
+$conn = new mysqli($server, $username, $password, $dbname);
+
+if($conn->connect_error)
+{
+	throw new Exception("Connection failed: " . $conn->connect_error);
+}
+
+
+
+$query = "SELECT * FROM bookingform WHERE 1";
+$result = $conn->query($query);
+
+if($result === false)
+{
+	throw new Exception("Query error: " . $conn->error);
+}
+
+if($result->num_rows > 0)
+{
+	echo("Rader finns");
+	while($row = $result->fetch_assoc())
+	{
+		//var_dump($row);
+		echo("<pre>");
+		print_r($row);
+		echo("</pre>");
+	}
+}
+else
+{
+	echo("Det finns inget att visa");
+}
+
+////////////////////////////// SERVER TEST //////////////////////////////////
+?>
         </div>
     </div>
     <footer class="footer">
