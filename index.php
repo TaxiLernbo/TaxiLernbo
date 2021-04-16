b3390f76afb9408a4871ab15f5e225d837d91657
Peter Ekholm <alexander.larsson.1@vbu.se>
2021-04-16 14:55:52 +0200

    Nya fält i formuläret

--- index.php
+++ index.php
@@ -50,7 +50,7 @@ $bookingForm->validateAndInsertForm();
                       &q=Lernbo+SW">
                   </iframe><br/><br/>
 
-                <label for="startPos">Din nuvarande position:</label><br/>
+                <label for="startPos"></label>
                 <input type="text" id="startPos" name="startPos" placeholder="Din nuvarande position:"/><br/><br/>
                 
                 <label for="endPos"></label><br/>
@@ -61,6 +61,35 @@ $bookingForm->validateAndInsertForm();
 
                 <label for="time"></label>
                 <input type="time" id="time" name="time"/><br/><br/>
+                
+                <label for="music">Musik och konversation:</label><br/>
+
+                <select id="music" name="music">
+                <option value="ingen musik">ingen musik</option>
+                <option value="80-tal">80-tal</option>
+                <option value="90-tal">90-tal</option>
+                <option value="Pop">Pop</option>
+                <option value="Rock">Rock</option>
+                <option value="Hip hop">Hip hop</option>
+                <option value="Klassisk">Klassisk</option>
+                <option value="Jazz">Jazz</option>
+                <option value="EDM">EDM</option>
+                <option value="Allsång">Allsång</option>
+                <option value="Metal">Metal</option>
+                <option value="K-pop">K-pop</option>
+                <option value="Schlager">Schlager</option>
+                <option value="Svensk">Svensk</option>
+                <option value="Blandat">Blandat</option>
+                </select>
+
+                <select id="conversation" name="conversation">
+                <option value="ingen konversation">ingen konversation</option>
+                <option value="konversation">konversation</option>
+                </select><br/><br/>
+
+
+
+
 
                 <label for="name">Ditt namn och mobilnummer:</label><br/>
                 <input type="text" id="name" name="name" placeholder="För och efternamn:"/>
