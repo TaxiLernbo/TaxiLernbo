ff1e838eafe1c964ae21489c5a3f6726d22f2ac5
Peter Ekholm <alexander.larsson.1@vbu.se>
2021-05-12 10:00:10 +0200

    Försvunnen fil tillbaka

---
+++ areas.php
@@ -0,0 +1,23 @@
+<?php
+
+require_once("include/CApp.php");
+
+?>
+
+<!DOCTYPE html>
+
+<?php
+    require_once("include\header.php");
+    renderHeader();
+?>
+
+    <div class="content">
+        <div id="main">
+            <h1> Bokningar</h1>
+
+        </div>
+    </div>
+    <?php
+    renderFooter();
+    ?>
+</html>
